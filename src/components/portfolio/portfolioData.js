4695fb1438117a9fb3d6e857c4a91b511c3b8252
Rafay <abdulrafaykhan857@gmail.com>
2023-09-27 18:41:50 +0500

    data chnge

--- src/components/portfolio/portfolioData.js
+++ src/components/portfolio/portfolioData.js
@@ -14,20 +14,13 @@ const portfolioData = [
     demo: "https://rafaykhan177.github.io/PAKPAY/",
   },
   {
-    id: 2,
-    image: IMG2,
-    title: "Old Portfolio",
-    github: "https://github.com/RafayKhan177/oldPortfolio",
-    demo: "https://rafaykhan177.github.io/oldPortfolio/",
-  },
-  {
     id: 3,
     image: IMG3,
     title: "Grow Your Business",
     github: "https://github.com/RafayKhan177/GrowYourBusiness",
     demo: "https://rafaykhan177.github.io/GrowYourBusiness/",
   },
-   {
+  {
     id: 4,
     image: IMG4,
     title: "Moxie",
@@ -40,6 +33,20 @@ const portfolioData = [
     title: "Stuffed",
     github: "https://github.com/RafayKhan177/stuffed",
     demo: "https://rafaykhan177.github.io/stuffed/",
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: "Not Shipito",
+    github: "https://github.com/RafayKhan177/r-NotShipito",
+    demo: "https://rafaykhan177.github.io/r-NotShipito/",
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: "Old Portfolio",
+    github: "https://github.com/RafayKhan177/oldPortfolio",
+    demo: "https://rafaykhan177.github.io/oldPortfolio/",
   }
 ];
 export default portfolioData;
